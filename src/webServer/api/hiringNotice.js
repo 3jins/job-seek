3a10b65414739b1bcc203f1cf1c41a9fbc865612
Sejin Jeon <jinaidy93@gmail.com>
2019-06-17 17:11:05 +0900

    Modify hiringNotice api

--- src/webServer/api/hiringNotice.js
+++ src/webServer/api/hiringNotice.js
@@ -3,23 +3,22 @@ import HiringNotice from '../../mongodb/model/HiringNotice';
 
 const router = express.Router();
 
-router.get('/:companyName?', (req, res) => {
-  const companyName = req.params.companyName || null;
+router.get('/', (req, res) => {
+  const company = req.query.company || null;
   const jobGroup = req.query.jobGroup || null;
   const category = req.query.category || null;
 
   const reqObj = {};
-  if (companyName !== null) reqObj.companyName = companyName;
+  if (company !== null) reqObj.companyName = company;
   if (jobGroup !== null) reqObj.jobGroup = jobGroup;
   if (category !== null) reqObj.category = category;
-  console.log(reqObj);
 
   return HiringNotice.find(reqObj)
     .then(hiringNoticeList => res.json(hiringNoticeList))
     .catch((err) => {
       console.error(err);
       return res.status(500).json({
-        message: 'Could not retrieve a hiringNotice',
+        message: 'Could not retrieve hiringNotices',
       });
     });
 });
