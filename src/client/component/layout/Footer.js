59dcc4cc8a38265ecf94e3f6ad592f493799c8ae
Sejin Jeon <jinaidy93@gmail.com>
2019-06-21 03:03:54 +0900

    Fill the footer

--- src/client/component/layout/Footer.js
+++ src/client/component/layout/Footer.js
@@ -3,6 +3,19 @@ import './Footer.scss';
 
 export default () => (
   <div className="footer">
-    <p>footer</p>
+    <div className="footer-wrapper">
+      <p>
+        <span>Bug Report / Request for Another Company : </span>
+        <span>
+        <a href="https://github.com/3jins/job-seek/issues">GitHub</a>
+      </span>
+      </p>
+      <p>
+        <span>Wanna know about the developer of this site? </span>
+        <span>
+        <a href="https://enhanced.kr">Visit here!</a>
+      </span>
+      </p>
+    </div>
   </div>
 );
