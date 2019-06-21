78dbea662aa3a307e32a8b51addddebc550a6fb1
Sejin Jeon <jinaidy93@gmail.com>
2019-06-21 18:47:12 +0900

    Hide menu button when it isn't needed

--- src/client/component/layout/TopBar.js
+++ src/client/component/layout/TopBar.js
@@ -3,18 +3,23 @@ import './TopBar.scss';
 
 export default (props) => {
   const { isMenuOpen, setIsMenuOpen } = props;
+  const curPath = window.location.href;
+  const needMenu = !['post'].includes(curPath.slice('https://'.length).split('/')[1]);
+
   return (
     <div className="top-bar">
       <div className="title-div">
         <h1><a href="/">JOB SEEK</a></h1>
       </div>
-      <div className="menu-button-div">
-        <div className={`menu-button ${isMenuOpen ? 'change' : ''}`} onClick={() => setIsMenuOpen(!isMenuOpen)}>
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>
+      {needMenu ? (
+        <div className="menu-button-div">
+          <div className={`menu-button ${isMenuOpen ? 'change' : ''}`} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
